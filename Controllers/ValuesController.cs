e6e59f73d782dc9ffc7b732f4d8437a5928ceda2
Simplevolk <simplevolk@gmail.com>
2020-08-13 22:32:15 +0300

    add async requests

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/controller")]
+    public class ValuesController: ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+           var values = await _context.Values.ToListAsync();
+           return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValues(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x=>x.Id==id);
+            return Ok(value);
+        }
+    }
+}
