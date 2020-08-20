78df8a7da654d4d4eaea532c5ef968fc8a9d7cba
Simplevolk <simplevolk@gmail.com>
2020-08-21 00:19:43 +0300

    add cors

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,8 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>(x=> x.UseSqlite
             (Configuration.GetConnectionString("DefaultConnection")));
-            services.AddControllers();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +44,7 @@ namespace DatingApp.API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x=>x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
