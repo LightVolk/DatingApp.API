eb289bf0a49614648bb49eb2e2c92968e1bb13bf
Simplevolk <simplevolk@gmail.com>
2020-08-31 22:39:03 +0300

    login

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,8 +1,12 @@
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
@@ -11,16 +15,24 @@ namespace DatingApp.API.Controllers
     public class AuthController:ControllerBase
     {
         private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
 
-        public AuthController(IAuthRepository repo)
+        public AuthController(IAuthRepository repo,IConfiguration config)
         {
             this._repo = repo;
+            this._config = config;
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
             // validation request
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
 
             userForRegisterDto.Username=userForRegisterDto.Username.ToLower();
 
@@ -36,6 +48,25 @@ namespace DatingApp.API.Controllers
 
             var createdUser = await _repo.Register(userToCreate,userForRegisterDto.Password);
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+           var userFromRepo = await _repo.Login(userForLoginDto.Username,userForLoginDto.Password);
+            if(userFromRepo == null)
+            {
+                return Unauthorized();
+            }
+
+            var claim = new []
+            {
+                new Claim(ClaimTypes.NameIdentifier,userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name,userFromRepo.Username)                
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha512Signature);
         }
 
 
